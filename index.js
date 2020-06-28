7f41e4944e58556109c5d424d4458c2380ceb633
Chetan <ckalia149@gmail.com>
2020-06-28 16:39:20 -0400

    added nav, footer, name_slide, jobs

---
+++ index.js
@@ -0,0 +1,23 @@
+      
+$(document).ready(function(){
+    $("#short_name").click(function(){
+        $("#hidden_description").slideDown("slow", function(){
+    $("#hidden_description").click(function(){
+    $(this).slideUp("slow");
+   
+    });
+    });
+    });
+    });
+
+//     $(window).scroll(function() {
+//   $(".slideanim").each(function(){
+//     var pos = $(this).offset().top;
+
+//     var winTop = $(window).scrollTop();
+//     if (pos < winTop + 600) {
+//       $(this).addClass("slide");
+//     }
+//   });
+// });
+
